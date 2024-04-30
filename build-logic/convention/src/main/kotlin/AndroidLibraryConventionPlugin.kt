efd3e7235b1a2d320498df01a70f89a4e3674841
Dhaval Shah <dhavalshah@gofynd.com>
2024-04-30 12:48:23 +0530

    Add android library convention plugin

---
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,35 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.gradle.convention.configureJavaAndroid
+import com.gradle.convention.configureKotlin
+import com.gradle.convention.findVersionIntExt
+import com.gradle.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        with(project) {
+            with(pluginManager) {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+                apply("org.jetbrains.kotlin.plugin.serialization")
+                apply("kotlin-parcelize")
+            }
+
+            extensions.configure<LibraryExtension> {
+                compileSdk = libs.findVersionIntExt("compileSdk")
+
+                defaultConfig {
+                    minSdk = libs.findVersionIntExt("minSdk")
+
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                }
+            }
+
+            configureJavaAndroid()
+            configureKotlin()
+        }
+    }
+}
--- build-logic/convention/src/main/kotlin/com/gradle/convention/Project+Extensions.kt
+++ build-logic/convention/src/main/kotlin/com/gradle/convention/Project+Extensions.kt
@@ -1,8 +1,19 @@
 package com.gradle.convention
 
+import com.android.build.api.dsl.AndroidResources
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.BuildFeatures
+import com.android.build.api.dsl.BuildType
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.DefaultConfig
+import com.android.build.api.dsl.LibraryExtension
+import com.android.build.api.dsl.ProductFlavor
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 internal val Project.libs
     get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
@@ -24,3 +35,37 @@ internal fun Project.debugImplementationExt(dependencyNotation: String) {
         add("debugImplementation", libs.findLibraryExt(dependencyNotation))
     }
 }
+
+internal fun Project.getCommonExtension(): CommonExtension<out BuildFeatures, out BuildType, out DefaultConfig, out ProductFlavor, out AndroidResources> {
+    val applicationExtension = try {
+        extensions.getByType<ApplicationExtension>()
+    } catch (e: Throwable) {
+        null
+    }
+
+    val libraryExtension = try {
+        extensions.getByType<LibraryExtension>()
+    } catch (e: Throwable) {
+        null
+    }
+
+    return (applicationExtension as? CommonExtension<*, *, *, *, *>)
+        ?: (libraryExtension as? CommonExtension<*, *, *, *, *>)
+        ?: throw Exception("Not able to find either Application or Library extension")
+}
+
+internal fun Project.configureKotlin() {
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_17.toString()
+        }
+    }
+}
+
+internal fun Project.configureJavaAndroid() {
+    getCommonExtension().compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
