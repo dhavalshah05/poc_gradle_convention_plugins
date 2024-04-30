e05f0d3759d687b3df11e26f6d4e18920b9c13ea
Dhaval Shah <dhavalshah@gofynd.com>
2024-04-30 09:33:15 +0530

    Setup project

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+rootProject.name = "poc_gradle_convention_plugins"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://www.jitpack.io") }
+    }
+}
+
+include(":app")
